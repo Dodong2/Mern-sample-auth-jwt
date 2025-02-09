d3739de48de7f87b70b153fbd94fe0d83cbfbb21
Dodong2 <carlseighartaliodegmail.com>
2025-02-09 17:18:59 +0800

    2 separate files

--- server/index.js
+++ server/index.js
@@ -9,9 +9,9 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: 'http://localhost:5173',
-    // methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    // allowedHeaders: ['Content-Type', 'Authorization'],
+    origin: 'https://samplepost.netlify.app',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true,
 }))
 app.use(cookieParcer())
